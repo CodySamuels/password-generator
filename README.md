cf6312b4d0cde9ed9ec91537b6c5e78d480adefc
Cody Samuels <twowoodenspoons@gmail.com>
2020-06-12 18:44:58 -0700

    updated readme and made a minor adjustment to the script that alerts users when they make invalid selections

--- README.md
+++ README.md
@@ -1,1 +1,49 @@
 # password-generator
+
+This is a very basic password generator. There was some provided code that basically when the generate password button is pressed, runs the functions, and then writes it to the text box.
+
+The top of the code has my arrays of global variables. There's an array for each character set.
+
+
+function generatePassword is the main code that I was required to write for the project. Inside the function we have some local variables
+variables 'finalPassword' and 'desiredChars' starts empty, but will be filled by the user's input.
+
+The first prompt asks if how long of a password they want generated. This is stored in an integer. There's a while loop to ensure that the user stays within the parameters of the assignment (8 to 128 characters).
+
+The next prompt asks if the user wants to use lowercase letters in their final password. This sets a boolean that if true concats the lowercase array into the desiredChars array. This is done for each of the character sets.
+
+NOTE: If the user doesn't set any parameters it will return undefined and alert them to try again. Ideally it would just start the process all over again, but that's not implemented yet.
+
+The next set of code is a loop that generates the final password. The loop runs a number of times equal to the user input. The result is then concated in the empty string finalPassword.
+
+Finally, finalPassword is displayed in the text of the html's card.
+
+
+
+## User Story
+
+```
+AS AN employee with access to sensitive data
+I WANT to randomly generate a password that meets certain criteria
+SO THAT I can create a strong password that provides greater security
+```
+
+## Acceptance Criteria
+
+```
+GIVEN I need a new, secure password
+WHEN I click the button to generate a password
+THEN I am presented with a series of prompts for password criteria
+WHEN prompted for password criteria
+THEN I select which criteria to include in the password
+WHEN prompted for the length of the password
+THEN I choose a length of at least 8 characters and no more than 128 characters
+WHEN prompted for character types to include in the password
+THEN I choose lowercase, uppercase, numeric, and/or special characters
+WHEN I answer each prompt
+THEN my input should be validated and at least one character type should be selected
+WHEN all prompts are answered
+THEN a password is generated that matches the selected criteria
+WHEN the password is generated
+THEN the password is either displayed in an alert or written to the page
+```
--- script.js
+++ script.js
@@ -1,49 +1,57 @@
-// Assignment Code
+// Assignment Code (was provided)
 var generateBtn = document.querySelector("#generate");
 
-// My various variables.
+// My various arrays for determing what characters to build the password out of.
 var specialChar = ['!', '@', '#', '$', '%', '^', '&', '*'];
 var numericChar = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0'];
 var lowercaseLetters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 var uppercaseLetters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+var tryAgain= "Please try again";
 
 
-
-// Create generatePassword (a function)
+// This function is the meat of the code.
 function generatePassword() {
+
+  // These variables are emtpy so that we can conccat data into them
   var finalPassword = '';
   var desiredChars = [];
 
-  // Prompt the User for password legnth, and store the password length in a variable.
+  // Prompt the User for password legnth, and store the password length in a variable. While loop prevents the user from entering an invalid password length.
   var passwordLength = prompt("How long do you want your password to be? Password must be 8-128 characters.");
   while (passwordLength < 8 || passwordLength > 128) {
     var passwordLength = prompt("Please only choose a value between 8 and 128.");
   }
   var passwordLength=parseInt(passwordLength);
   
-  // Confirm whether the user wants lowercase characters. Stores that in a boolean, and if so pushes those characters into desiredChars.
+  // Confirm whether the user wants lowercase characters. Stores that in a boolean, and if so concats those characters into desiredChars.
   var includeLowercasePassword = confirm("Do you want lowercase letters in your password?");
   if (includeLowercasePassword === true) {
     desiredChars = desiredChars.concat(lowercaseLetters);
   };
 
-  // Confirm whether the user wants uppercase characters. Stores that in a boolean, and if so pushes those characters into desiredChars.
+  // Confirm whether the user wants uppercase characters. Stores that in a boolean, and if so concats those characters into desiredChars.
   var includeUppercasePassword = confirm("Do you want uppercase letters in your password?");
   if (includeUppercasePassword === true) {
     desiredChars = desiredChars.concat(uppercaseLetters);
   };
 
-  // Confirm whether the user wants special characters. Stores that in a boolean, and if so pushes those characters into desiredChars.
+  // Confirm whether the user wants special characters. Stores that in a boolean, and if so concats those characters into desiredChars.
   var includeSpecialPassword = confirm("Do you want special characters in your password?");
   if (includeSpecialPassword === true) {
     desiredChars = desiredChars.concat(specialChar);
   };
 
-  // Confirm whether the user wants numeric characters. Stores that in a boolean, and if so pushes those characters into desiredChars.
+  // Confirm whether the user wants numeric characters. Stores that in a boolean, and if so concats those characters into desiredChars.
   var includeNumericPassword = confirm("Do you want numeric characters in your password?");
   if (includeNumericPassword === true) {
     desiredChars = desiredChars.concat(numericChar);
   };
+
+  // Asks to user to try again if they don't accept any parameters for password generation.
+  if (includeLowercasePassword !==true && includeUppercasePassword !==true && includeNumericPassword!==true && includeSpecialPassword !==true) {
+    alert ("Please select a character set next time.");
+    return
+  }
 
   // This generates one character of the password. This loops a number of times equal to passwordLength.
   for (let i = 0; i < passwordLength; i++) {
